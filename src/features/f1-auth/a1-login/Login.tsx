cb13deb0cb6f176b0f0f2931dbeead0873905c2a
Italia1235 <1437dada@mail.ru>
2022-01-11 22:33:37 +0600

    html/css add page Login

--- src/features/f1-auth/a1-login/Login.tsx
+++ src/features/f1-auth/a1-login/Login.tsx
@@ -1,7 +1,29 @@
+import SuperInput from "../../../main/ui/common/superInput/SuperInput";
+import s from './Login.tsx.module.css'
+import {useState} from "react";
 export const Login = () => {
-    return (
-        <h3>
-           Login
-        </h3>
+
+    const [emailValue,setEmailValue] = useState()
+    const [passValue,setPassValue] = useState()
+
+
+        return (
+
+
+
+        <div >
+            <h2 style ={{textAlign:"center",paddingTop:"10px" }}>It-incubator</h2>
+            <div className={s.allLoginArea}>
+
+        <h3>Sign In</h3>
+
+            <form>
+            <p>Email</p>
+            <SuperInput/>
+            <p>Password</p>
+            <SuperInput />
+            </form>
+            </div>
+        </div>
     )
 }
