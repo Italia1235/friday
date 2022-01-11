e5fb658bb2480e33ab6bd02d37253639001e2a4c
Italia1235 <1437dada@mail.ru>
2022-01-11 23:18:05 +0600

    login state page

--- src/features/f1-auth/a1-login/Login.tsx
+++ src/features/f1-auth/a1-login/Login.tsx
@@ -1,15 +1,21 @@
 import SuperInput from "../../../main/ui/common/superInput/SuperInput";
 import s from './Login.tsx.module.css'
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
+import SuperButton from "../../../main/ui/common/superButton/SuperButton";
+import {Link, NavLink} from "react-router-dom";
+import {PATH} from "../../../main/ui/routes/Routes";
 export const Login = () => {
 
-    const [emailValue,setEmailValue] = useState()
-    const [passValue,setPassValue] = useState()
+    const [emailValue,setEmailValue] = useState<string>("")
+    const [passValue,setPassValue] = useState<string>("")
 
-
+    const changePasswordValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassValue(e.currentTarget.value)
+    }
+    const changeEmailValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmailValue(e.currentTarget.value)
+    }
         return (
-
-
 
         <div >
             <h2 style ={{textAlign:"center",paddingTop:"10px" }}>It-incubator</h2>
@@ -19,11 +25,18 @@ export const Login = () => {
 
             <form>
             <p>Email</p>
-            <SuperInput/>
+            <SuperInput onChange={changeEmailValue} value={emailValue}/>
             <p>Password</p>
-            <SuperInput />
+            <SuperInput onChange={changePasswordValue} value={passValue} />
+
+
             </form>
+                <div style={{paddingTop:"10px"}}>  <NavLink style={{color:"darkgrey",paddingLeft:"250px"}} to={PATH.FORGOT}>Forgot Password?</NavLink> </div>
+                <SuperButton  style={{marginTop:"10px",width:"80px"}}>Login</SuperButton>
+
+                <SuperButton style={{marginTop:"10px",width:"80px"}}>Sign up</SuperButton>
             </div>
+
         </div>
     )
 }
