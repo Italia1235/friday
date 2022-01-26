84c8906b305a603e0f1727bab6f2174ae821cc4e
zillomoons <dlzht73@ya.ru>
2022-01-27 00:22:11 +0300

    created modal component

--- src/features/f1-auth/a1-login/Login.tsx
+++ src/features/f1-auth/a1-login/Login.tsx
@@ -1,22 +1,22 @@
 import SuperInput from "../../../main/ui/common/superInput/SuperInput";
 import s from './Login.tsx.module.css'
-import {ChangeEvent, useState} from "react";
+import { ChangeEvent, useState } from "react";
 import SuperButton from "../../../main/ui/common/superButton/SuperButton";
-import {Link, Navigate, NavLink} from "react-router-dom";
-import {PATH} from "../../../main/ui/routes/Routes";
-import {authReducer, LoginPageType, LoginThunkCreator} from "../../../main/bll/reducers/login-reducer";
-import {useDispatch, useSelector} from "react-redux";
-import {AppStoreType} from "../../../main/bll/store/store";
-import {InitStateTypeProfile} from "../../../main/bll/reducers/profile-reducer";
+import { Link, Navigate, NavLink } from "react-router-dom";
+import { PATH } from "../../../main/ui/routes/Routes";
+import { authReducer, LoginPageType, LoginThunkCreator } from "../../../main/bll/reducers/login-reducer";
+import { useDispatch, useSelector } from "react-redux";
+import { AppStoreType } from "../../../main/bll/store/store";
+import { InitStateTypeProfile } from "../../../main/bll/reducers/profile-reducer";
 
 export const Login = () => {
     const login = useSelector<AppStoreType, LoginPageType>(state => state.login)
-    const {isLoading, appError} = useSelector((state: AppStoreType) => state.app);
+    const { isLoading, appError } = useSelector((state: AppStoreType) => state.app);
     const [emailValue, setEmailValue] = useState<string>("")
     const [passValue, setPassValue] = useState<string>("")
     const dispatch = useDispatch();
     const authStart = () => {
-        dispatch(LoginThunkCreator({email: emailValue, password: passValue, rememberMe: true}))
+        dispatch(LoginThunkCreator({ email: emailValue, password: passValue, rememberMe: true }))
     }
 
     const changePasswordValue = (e: ChangeEvent<HTMLInputElement>) => {
@@ -27,29 +27,29 @@ export const Login = () => {
     }
 
     if (appError == null && login.email !== "") {
-        debugger
-        return <Navigate to={"/profile"}/>
+
+        return <Navigate to={"/profile"} />
     }
 
 
     return (
 
         <div>
-            <h2 style={{textAlign: "center", paddingTop: "10px"}}>It-incubator</h2>
+            <h2 style={{ textAlign: "center", paddingTop: "10px" }}>It-incubator</h2>
             <div className={s.allLoginArea}>
                 <h3>Sign In</h3>
                 <form>
                     <p>Email</p>
-                    <SuperInput onChange={changeEmailValue} value={emailValue}/>
+                    <SuperInput onChange={changeEmailValue} value={emailValue} />
                     <p>Password</p>
-                    <input type="password" onChange={changePasswordValue} value={passValue}/>мваd
+                    <input type="password" onChange={changePasswordValue} value={passValue} />мваd
 
-                    {appError && <div style={{color: "red"}}>{appError}</div>}
+                    {appError && <div style={{ color: "red" }}>{appError}</div>}
                 </form>
-                <div style={{paddingTop: "10px"}}><NavLink style={{color: "darkgrey", paddingLeft: "250px"}}
-                                                           to={PATH.FORGOT}>Forgot Password?</NavLink></div>
-                <SuperButton style={{marginTop: "10px", width: "80px"}} onClick={authStart}>Login</SuperButton>
-                <SuperButton style={{marginTop: "10px", width: "80px"}} > Sign up</SuperButton>
+                <div style={{ paddingTop: "10px" }}><NavLink style={{ color: "darkgrey", paddingLeft: "250px" }}
+                    to={PATH.FORGOT}>Forgot Password?</NavLink></div>
+                <SuperButton style={{ marginTop: "10px", width: "80px" }} onClick={authStart}>Login</SuperButton>
+                <SuperButton style={{ marginTop: "10px", width: "80px" }} > Sign up</SuperButton>
             </div>
 
         </div>
